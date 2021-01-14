2612c50aa7ab818b7751f01402cb9bbeb5eb2ffd
Eliott Teissonniere <git.eliott@teissonniere.org>
2021-01-14 10:53:25 +0100

    offchain claims with no security checks

--- contracts/CapsuleCoin.sol
+++ contracts/CapsuleCoin.sol
@@ -1,11 +1,74 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.7.0;
 
+import "@openzeppelin/contracts/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+/*
+ * Capsule Coin is the ERC20 token for the Ternoa platform. It is received and
+ * used by early supporters while waiting for the Ternoa chain to be released.
+ * Once the Ternoa Chain is live people will have a way to convert their ERC20s
+ * to the Ternoa Chain native asset.
+ *
+ * It should act as a standard ERC20, along with some niceties (read below).
+ *
+ * Accounts have the ability to sign off-chain "proofs" that can be used by other
+ * accounts to do the equivalent of a `transferFrom` operation. This is mostly used
+ * for granting coins to presale investors. Indeed, this avoids us having to create
+ * hundreds of transactions or deploy a heavy contract just to do so. Instead, people
+ * will be able to fetch their "proof of claim" and submit it against the contract to
+ * retrieve their coins.
+ * A proof of claim is composed of:
+ * - the source and destination accounts.
+ * - the amount of coins involved.
+ * - a block after which it can be executed, if the proof is submitted too early it should
+ *   be rejected.
+ * - a salt used to avoid replay attacks, if the salt was already seen for the same source
+ *   account the proof should be discarded.
+ */
 contract CapsuleCoin is ERC20 {
+    using ECDSA for bytes32;
+
+    mapping(address => mapping(uint256 => bool)) public saltUsed;
+
     constructor(address vault) public ERC20("CapsuleCoin", "CACO") {
         // We create 2.5B coins but the token has 18 decimals.
         _mint(vault, 2500000000 * (10 ^ 18));
+    }
+
+    /*
+     * @description Create the hash that needs to be signed to create a claimable proof.
+     * @param from The account we will claim funds from.
+     * @param to The account that will receive the funds.
+     * @param amount The amount of coins we will transfer.
+     * @param validity The block number after which we can use the proof.
+     * @param salt A unique value to avoid replay attacks.
+     * @return The hash that needs to be signed by the from account.
+     */
+    function hashForClaim(
+        address from,
+        address to,
+        uint256 amount,
+        uint256 validity,
+        uint256 salt
+    ) public pure returns (bytes32) {
+        return
+            keccak256(abi.encodePacked(from, to, amount, validity, salt))
+                .toEthSignedMessageHash();
+    }
+
+    /*
+     * @description Verify a proof for a claim and execute it if it is valid.
+     */
+    function claimOffchainGrant(
+        bytes memory proof,
+        address from,
+        address to,
+        uint256 amount,
+        uint256 validity,
+        uint256 salt
+    ) public {
+        _transfer(from, to, amount);
+        saltUsed[from][salt] = true;
     }
 }
--- test/CapsuleCoin.js
+++ test/CapsuleCoin.js
@@ -2,24 +2,70 @@ const { expect } = require("chai");
 
 describe("Capsule Coin contract", () => {
     let owner;
+    let user;
     let token;
 
     beforeEach(async () => {
         const CapsuleCoin = await ethers.getContractFactory("CapsuleCoin");
-        const [deployer] = await ethers.getSigners();
+        [owner, user] = await ethers.getSigners();
 
-        token = await CapsuleCoin.deploy(deployer.address);
-        owner = deployer;
+        token = await CapsuleCoin.deploy(owner.address);
     })
 
-    it("Deployment should assign the total supply of tokens to the specified address", async () => {
-        const ownerBalance = await token.balanceOf(owner.address);
-        expect(await token.totalSupply()).to.equal(ownerBalance);
-    });
+    context("Deployment", () => {
+        it("Deployment should assign the total supply of tokens to the specified address", async () => {
+            const ownerBalance = await token.balanceOf(owner.address);
+            expect(await token.totalSupply()).to.equal(ownerBalance);
+        });
 
-    it("Has a total supply of 25B tokens", async () => {
-        const oneBillion = 1000000000;
-        const decimals = await token.decimals();
-        expect(await token.totalSupply()).to.equal(2.5 * oneBillion * (10 ^ decimals));
+        it("Has a total supply of 25B tokens", async () => {
+            const oneBillion = 1000000000;
+            const decimals = await token.decimals();
+            expect(await token.totalSupply()).to.equal(2.5 * oneBillion * (10 ^ decimals));
+        })
+    })
+
+    context("Offchain Claim", () => {
+        const targetAmount = 10000;
+        const salt = 1;
+
+        let createClaim = async (destination, amount, forBlock, salt) => {
+            let hash = await token.hashForClaim(owner.address, destination, amount, forBlock, salt);
+            let signed = owner.signMessage(hash);
+
+            return signed;
+        }
+
+        context("Valid", () => {
+            it("Transfer the coins", async () => {
+                const claim = await createClaim(user.address, targetAmount, ethers.provider.blockNumber, salt);
+
+                await token
+                    .connect(user)
+                    .claimOffchainGrant(claim, owner.address, user.address, targetAmount, ethers.provider.blockNumber, salt);
+
+                const totalSupply = await token.totalSupply();
+                expect(await token.balanceOf(owner.address)).to.equal(totalSupply - targetAmount);
+                expect(await token.balanceOf(user.address)).to.equal(targetAmount);
+            })
+
+            it("Marks the salt as used", async () => {
+                const salt = 1;
+                const claim = await createClaim(user.address, targetAmount, ethers.provider.blockNumber, salt);
+
+                await token
+                    .connect(user)
+                    .claimOffchainGrant(claim, owner.address, user.address, targetAmount, ethers.provider.blockNumber, salt);
+
+                expect(await token.saltUsed(owner.address, salt)).to.equal(true);
+            })
+        })
+
+        context("Invalid", () => {
+            it("Refuse already used salt")
+
+            it("Refuse if submitted too early")
+            it("Refuse bad signature")
+        })
     })
 });
